31eab90af2a07fa8ede21cd8c0745a393c7c5d7e
StarLuminary <paulalexism@gmail.com>
2025-08-01 12:05:52 -0600

    add object constructor for books

---
+++ script.js
@@ -0,0 +1,32 @@
+const myLibrary = [dune];
+const submit = document.getElementById('submitButton')
+
+submit.addEventListener('click',()=> submit.textContent='uwu')
+
+const dune = {
+    author:'Frank Herbert',
+    title:'Dune',
+    pages:676,
+    year:1969,
+    id:1,
+ }
+
+function Book(author, title, pages, year) {
+  // the constructor...
+  this.author=author
+  this.title=title
+  this.pages=pages
+  this.year=year
+  this.id=crypto.randomUUID()
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+  let newBook= new Book(author, title, pages, year)
+  myLibrary.push(newBook)
+}
+
+function displayBooks(){
+    //displays the books in the array
+    document.createElement 
+}
